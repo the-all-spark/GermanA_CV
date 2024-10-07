73966a26c411ae0ea8ac5515723c4882da7133cd
Anna <anna.r.german1410@gmail.com>
2024-10-07 20:28:16 +0300

    feat: add function to hide info about project when clicking on hide button

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ window.onload = function() {
 
                 let hideBtn = document.querySelector('.hide-more-btn[data-project = "' + chosenProject + '"]');
                 hideBtn.style.display = "block";
-                //! добавить обработчик события закрытия блока при клике на кнопку Скрыть
+                hideBtn.addEventListener("click", function() { hideMoreInfo(this,chosenProject) });
 
                 this.style.display = "none";
             }
@@ -31,6 +31,16 @@ window.onload = function() {
 
     }
 
+    // * функция скрытия доп.информации при клике на кнопку "Скрыть"
+    // принимает кнопку, на которую кликнули и значение атрибута data-project 
+    function hideMoreInfo(btn,project) {
+        btn.style.display = "none";
+        document.querySelector('.more-block[data-project = "' + project + '"]').style.display = "none";
+        document.querySelector('.show-more-btn[data-project = "' + project + '"]').style.display = "block";
+    }
+
+
+    // TODO сделать кнопки показать подробнее все (вверху) скрыть подробнее все - внизу (блок Опыт)
 
 
 
